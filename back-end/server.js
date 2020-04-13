2adae9bebd3a74c545e505217180c9f7e794c093
BZIvanov <biser.z.ivanov@gmail.com>
2020-04-13 22:27:24 +0300

    tours added

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3100;
 app.listen(port, () => {
